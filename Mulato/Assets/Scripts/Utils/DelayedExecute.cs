801d2ca8c2f556900eb5a2c4f9331d2263fbeac5
AvivLuria <avivluria@gmail.com>
2016-02-26 12:21:33 +0200

    Added delayed execute utility script

---
+++ Mulato/Assets/Scripts/Utils/DelayedExecute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public static class DelayedExecute
+{
+    public static void ExecuteWithDelay(this MonoBehaviour behavior, float delay, Action action) {
+        if (action != null) {
+            behavior.StartCoroutine(DelayedExecutionCoroutine(delay, action));
+        }
+    }
+
+    private static IEnumerator DelayedExecutionCoroutine(float delay, Action action) {
+        yield return new WaitForSeconds(delay);
+        action();
+    }
+}
+
+
+

1f80e6e6cdaaae065318ba3f5db7aca8846a884d
dotan liba <dotan liba>
2016-06-14 17:14:22 +0300

    s

--- Mulato/Assets/Scripts/UI.cs
+++ Mulato/Assets/Scripts/UI.cs
@@ -11,7 +11,7 @@ public class Ui : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && click)
         {
             //get the hit position
-            m_inputMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+             m_inputMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var hit = Physics2D.Raycast(m_inputMouse, Vector2.zero);
             if (hit.collider != null && hit.collider.tag.Equals("floor") && GameManager.main.m_CurrentBoard.GetComponent<BoardManager>().GetGridPointObject
                 (hit.collider.gameObject.GetComponent<floor>().gridRow, 
